99841b7bf6b5913c0bf7404406162575c4742ecd
MileshPatel28 <milesh.patel28@gmail.com>
2025-10-08 15:13:08 -0400

    added transparent background for canvas

--- src/app/mainCanvas.tsx
+++ src/app/mainCanvas.tsx
@@ -9,28 +9,34 @@ export default function MainCanvas() {
         const mainCanvas = document.getElementById('mainScene')
 
         if(mainCanvas != null){
-        
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+            const scene = new THREE.Scene();
+            const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-        const renderer = new THREE.WebGLRenderer({canvas: mainCanvas});
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        
-        
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshBasicMaterial( { color: 'rgb(255,255,255)' } );
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
-        
-        camera.position.z = 5;
+            const renderer = new THREE.WebGLRenderer({canvas: mainCanvas,alpha: true});
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            
+            
+            const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+            const material = new THREE.MeshBasicMaterial( { color: 'rgb(255,255,255)' } );
+            const cube = new THREE.Mesh( geometry, material );
+            scene.add( cube );
+            
+            camera.position.z = 5;
 
-        renderer.render(scene, camera)
+            renderer.render(scene, camera)
         }
         
     }, [])
 
     return(
-        <canvas id="mainScene" style={{background: 'rgb(21,21,21)', width: '100%' }}></canvas>
+        <canvas 
+        id="mainScene" 
+        style={{
+            margin: '0 !important',
+            padding: '0 !important',
+            position: 'fixed'
+        }}
+        />
     )
 }
