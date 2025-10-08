6a9a944d4a63953e5f36c236f7848f55b71d2f64
MileshPatel28 <milesh.patel28@gmail.com>
2025-10-08 14:38:02 -0400

    code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import * as THREE from 'three';
 export default function Home() {
 
   useEffect(() => {
-
-    
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    
-    
     const mainCanvas = document.getElementById('mainScene')
+
     if(mainCanvas != null){
+      
+
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
       const renderer = new THREE.WebGLRenderer({canvas: mainCanvas});
       renderer.setSize( window.innerWidth, window.innerHeight );
       
